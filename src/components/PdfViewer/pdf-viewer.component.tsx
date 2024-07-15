d5c63638d00af84e98d6226bbc2e5e13d2ff5773
David <d.graham8@rgu.ac.uk>
2024-07-15 13:07:35 +0100

    add virtual scrolling to improve performance

--- src/components/PdfViewer/pdf-viewer.component.tsx
+++ src/components/PdfViewer/pdf-viewer.component.tsx
@@ -1,14 +1,18 @@
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/Page/TextLayer.css";
 import "react-pdf/dist/Page/AnnotationLayer.css";
-import { createContext, useState } from "react";
+import { createContext, useRef, useState } from "react";
 import { PdfViewerMenu } from "./pdf-viewer-menu.component";
+import { useVirtualizer } from "@tanstack/react-virtual";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
   import.meta.url,
 ).toString();
 
+const MENU_HEIGHT = 40;
+const PADDING = 32;
+const VIEWPORT_HEIGHT = window.innerHeight - MENU_HEIGHT;
 const PAGE_HEIGHT = window.outerHeight;
 const options = { cMapUrl: "/cmaps/" };
 
@@ -22,11 +26,23 @@ export function PdfViewer() {
   const [numPages, setNumPages] = useState<number>(0);
   const [pageNumber, setPageNumber] = useState<number>(1);
   const contextValue = { numPages, pageNumber, setPageNumber };
-  const iteratePages = Array.from({ length: numPages });
+  // const iteratePages = Array.from({ length: numPages });
+  const scrollRef = useRef<HTMLDivElement>(null);
 
   function onLoaded({ numPages }: { numPages: number }) {
     setNumPages(numPages);
   }
+
+  const virtualList = useVirtualizer({
+    count: numPages,
+    getScrollElement: () => scrollRef.current,
+    estimateSize: () => PAGE_HEIGHT,
+    gap: 16,
+    paddingStart: PADDING,
+    paddingEnd: PADDING,
+    overscan: 5,
+    scrollMargin: 0,
+  });
 
   return (
     <>
@@ -37,20 +53,33 @@ export function PdfViewer() {
       <Document
         options={options}
         file="/sample.pdf"
-        className="flex h-[calc(100vh_-_40px)] flex-col items-center gap-4 overflow-auto bg-gray-400 p-8"
+        className="bg-gray-400"
         onLoadSuccess={onLoaded}
-        onScroll={(e) => {
-          console.log(e);
-        }}
+        // inputRef={scrollRef} // <--- experiment with this
       >
-        {iteratePages.map((_, index) => (
-          <Page
-            key={index}
-            className="shadow-sm"
-            pageNumber={index + 1}
-            height={PAGE_HEIGHT}
-          />
-        ))}
+        <div
+          ref={scrollRef}
+          className="overflow-auto"
+          style={{ height: `${VIEWPORT_HEIGHT}px` }}
+        >
+          <div
+            className="relative w-full"
+            style={{ height: `${virtualList.getTotalSize()}px` }}
+          >
+            {virtualList.getVirtualItems().map((item) => (
+              <div
+                key={item.key}
+                className="absolute left-0 top-0 flex w-full justify-center"
+                style={{
+                  height: `${item.size}px`,
+                  transform: `translateY(${item.start}px)`,
+                }}
+              >
+                <Page height={item.size} pageNumber={item.index + 1} />
+              </div>
+            ))}
+          </div>
+        </div>
       </Document>
     </>
   );
