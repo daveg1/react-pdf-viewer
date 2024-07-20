7c8d6d0f3d4f3cb8e38110b8081fd16ee086dc2a
David <d.graham8@rgu.ac.uk>
2024-07-20 23:06:16 +0100

    add translate button

--- src/PdfViewer/components/PdfSidebar.tsx
+++ src/PdfViewer/components/PdfSidebar.tsx
@@ -50,7 +50,9 @@ export function PdfSidebar() {
       rel={isSidebarOpen ? "open" : "closed"}
       className="Sidebar w-full max-w-sm select-none bg-gray-300 p-4"
     >
-      <h3 className="mb-4 text-xl font-medium leading-tight">Bookmarks</h3>
+      <h3 className="mb-4 text-xl font-medium leading-tight">
+        Bookmarked Phrases
+      </h3>
 
       <div className="flex flex-col gap-4">
         {Object.entries(groupBookmarks(bookmarks)).map(
@@ -116,6 +118,30 @@ export function PdfSidebar() {
 
                     <button
                       className="ml-auto p-1 hover:bg-black/10"
+                      type="button"
+                      onClick={() => {
+                        window.open(
+                          `https://translate.google.co.uk/?sl=auto&tl=en&op=translate&text=${encodeURIComponent(bookmark.selectedText)}`,
+                          "_blank",
+                        );
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="size-4"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M11 5a.75.75 0 0 1 .688.452l3.25 7.5a.75.75 0 1 1-1.376.596L12.89 12H9.109l-.67 1.548a.75.75 0 1 1-1.377-.596l3.25-7.5A.75.75 0 0 1 11 5Zm-1.24 5.5h2.48L11 7.636 9.76 10.5ZM5 1a.75.75 0 0 1 .75.75v1.261a25.27 25.27 0 0 1 2.598.211.75.75 0 1 1-.2 1.487c-.22-.03-.44-.056-.662-.08A12.939 12.939 0 0 1 5.92 8.058c.237.304.488.595.752.873a.75.75 0 0 1-1.086 1.035A13.075 13.075 0 0 1 5 9.307a13.068 13.068 0 0 1-2.841 2.546.75.75 0 0 1-.827-1.252A11.566 11.566 0 0 0 4.08 8.057a12.991 12.991 0 0 1-.554-.938.75.75 0 1 1 1.323-.707c.049.09.099.181.15.271.388-.68.708-1.405.952-2.164a23.941 23.941 0 0 0-4.1.19.75.75 0 0 1-.2-1.487c.853-.114 1.72-.185 2.598-.211V1.75A.75.75 0 0 1 5 1Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
+
+                    <button
+                      className="p-1 hover:bg-black/10"
                       type="button"
                       onClick={() =>
                         scrollToPage({
