aebdccb1560ff5b93bd9e94287ac0e1a22b2562f
David <d.graham8@rgu.ac.uk>
2024-07-20 22:39:02 +0100

    restyle bookmarks

--- src/PdfViewer/components/PdfSidebar.tsx
+++ src/PdfViewer/components/PdfSidebar.tsx
@@ -57,7 +57,7 @@ export function PdfSidebar() {
           ([pageIndex, bookmarks]) => (
             <section key={pageIndex}>
               <h3
-                className="group flex cursor-pointer items-center gap-1 rounded-sm px-1 hover:bg-black/10"
+                className="group flex cursor-pointer items-center gap-1 rounded-sm px-2 py-1 hover:bg-black/10"
                 onClick={() =>
                   scrollToPage({
                     pageNumber: +pageIndex + 1,
@@ -81,42 +81,64 @@ export function PdfSidebar() {
               </h3>
 
               {bookmarks.map((bookmark) => (
-                <div
+                <article
                   key={bookmark.key}
-                  className="flex cursor-pointer select-none items-center gap-2 rounded-sm px-2 py-1 hover:bg-black/10"
+                  className="flex cursor-pointer select-none items-center gap-2 rounded p-2 hover:bg-black/10"
                   onMouseOver={() => highlightBookmark(bookmark.key)}
                   onMouseLeave={() => resetHighlights()}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16"
-                    fill="currentColor"
-                    className="group size-4 text-gray-500"
-                    onClick={() => removeBookmark(bookmark.key)}
-                  >
-                    <path
-                      className="group-hover:hidden"
-                      d="M3.75 2a.75.75 0 0 0-.75.75v10.5a.75.75 0 0 0 1.28.53L8 10.06l3.72 3.72a.75.75 0 0 0 1.28-.53V2.75a.75.75 0 0 0-.75-.75h-8.5Z"
-                    />
+                  <div className="flex w-full items-center gap-2">
+                    <button
+                      type="button"
+                      onClick={() => removeBookmark(bookmark.key)}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="group size-4 text-gray-500"
+                      >
+                        <path
+                          className="group-hover:hidden"
+                          d="M3.75 2a.75.75 0 0 0-.75.75v10.5a.75.75 0 0 0 1.28.53L8 10.06l3.72 3.72a.75.75 0 0 0 1.28-.53V2.75a.75.75 0 0 0-.75-.75h-8.5Z"
+                        />
 
-                    <path
-                      className="hidden group-hover:block group-hover:text-gray-900"
-                      d="M13 2.75v7.775L4.475 2h7.775a.75.75 0 0 1 .75.75ZM3 13.25V5.475l4.793 4.793L4.28 13.78A.75.75 0 0 1 3 13.25ZM2.22 2.22a.75.75 0 0 1 1.06 0l10.5 10.5a.75.75 0 1 1-1.06 1.06L2.22 3.28a.75.75 0 0 1 0-1.06Z"
-                    />
-                  </svg>
+                        <path
+                          className="hidden group-hover:block group-hover:text-gray-900"
+                          d="M13 2.75v7.775L4.475 2h7.775a.75.75 0 0 1 .75.75ZM3 13.25V5.475l4.793 4.793L4.28 13.78A.75.75 0 0 1 3 13.25ZM2.22 2.22a.75.75 0 0 1 1.06 0l10.5 10.5a.75.75 0 1 1-1.06 1.06L2.22 3.28a.75.75 0 0 1 0-1.06Z"
+                        />
+                      </svg>
+                    </button>
 
-                  <span
-                    className="-my-1 -mr-2 w-full max-w-sm overflow-hidden text-ellipsis whitespace-nowrap py-1 pr-2"
-                    onClick={() =>
-                      scrollToPage({
-                        offset:
-                          bookmark.scrollPercent * virtualList.getTotalSize(),
-                      })
-                    }
-                  >
-                    {bookmark.selectedText}
-                  </span>
-                </div>
+                    <span className="overflow-hidden text-ellipsis whitespace-nowrap">
+                      {bookmark.selectedText}
+                    </span>
+
+                    <button
+                      className="ml-auto p-1 hover:bg-black/10"
+                      type="button"
+                      onClick={() =>
+                        scrollToPage({
+                          offset:
+                            bookmark.scrollPercent * virtualList.getTotalSize(),
+                        })
+                      }
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 16 16"
+                        fill="currentColor"
+                        className="size-4"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          d="M12.78 7.595a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06l2.72-2.72-2.72-2.72a.75.75 0 0 1 1.06-1.06l3.25 3.25Zm-8.25-3.25 3.25 3.25a.75.75 0 0 1 0 1.06l-3.25 3.25a.75.75 0 0 1-1.06-1.06l2.72-2.72-2.72-2.72a.75.75 0 0 1 1.06-1.06Z"
+                          clipRule="evenodd"
+                        />
+                      </svg>
+                    </button>
+                  </div>
+                </article>
               ))}
             </section>
           ),
